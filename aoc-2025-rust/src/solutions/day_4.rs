f757c568b8985a1ef4af06e86e6e40148d7f3b4f
Mark Williams <amwilliams137@gmail.com>
2025-12-08 08:46:30 +0000

    4.1

--- aoc-2025-rust/src/solutions/day_4.rs
+++ aoc-2025-rust/src/solutions/day_4.rs
@@ -1,7 +1,7 @@
 use crate::solutions::file_helpers::get_lines_from_file;
 
 pub fn part_1() -> u32 {
-    let lines = get_lines_from_file("../inputs/4.example.txt");
+    let lines = get_lines_from_file("../inputs/4.txt");
 
     let width = lines.first().unwrap().len();
     let height = lines.len();
@@ -13,19 +13,19 @@ pub fn part_1() -> u32 {
 
     let mut accessible_rolls_count = 0;
 
-    let mut x = 0;
-    while x < width {
-        let mut y = 0;
+    let mut y = 0;
+    while y < height {
+        let mut x = 0;
 
-        while y < height {
-            if grid[y][x] == '@' && count_rolls_in_adjacent_positions(x, y, &grid) < 4 {
+        while x < width {
+            if grid[y][x] == '@' && !has_too_many_rolls_in_adjacent_positions(x, y, &grid) {
                 accessible_rolls_count += 1;
             }
 
-            y += 1;
+            x += 1;
         }
 
-        x += 1;
+        y += 1;
     }
 
     accessible_rolls_count
@@ -35,6 +35,42 @@ pub fn part_2() -> u32 {
     0
 }
 
-fn count_rolls_in_adjacent_positions(x: usize, y: usize, grid: &Vec<Vec<char>>) -> u32 {
-    0
+fn has_too_many_rolls_in_adjacent_positions(
+    x_centre: usize,
+    y_centre: usize,
+    grid: &Vec<Vec<char>>,
+) -> bool {
+    let mut adjacent_roll_count = 0;
+
+    let mut y = if y_centre == 0 { 0 } else { y_centre - 1 };
+
+    while y <= y_centre + 1 {
+        let mut x = if x_centre == 0 { 0 } else { x_centre - 1 };
+
+        while x <= x_centre + 1 {
+            if (x != x_centre || y != y_centre) && is_position_roll(x, y, grid) {
+                adjacent_roll_count += 1;
+            }
+
+            if adjacent_roll_count >= 4 {
+                return true;
+            }
+
+            x += 1;
+        }
+
+        y += 1;
+    }
+
+    return false;
+}
+
+fn is_position_roll(x: usize, y: usize, grid: &Vec<Vec<char>>) -> bool {
+    match grid.get(y) {
+        None => false,
+        Some(row) => match row.get(x) {
+            Some('@') => true,
+            _ => false,
+        },
+    }
 }
