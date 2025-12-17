4b857d950c5cdcbd446a7660a6befa28f7802334
Mark Williams <amwilliams137@gmail.com>
2025-12-17 18:55:25 +0000

    5.1 wow first try

--- aoc-2025-rust/src/solutions/day_5.rs
+++ aoc-2025-rust/src/solutions/day_5.rs
@@ -18,8 +18,71 @@ pub fn part_1() -> u32 {
     result
 }
 
-pub fn part_2() -> u32 {
-    0
+pub fn part_2() -> u64 {
+    let (id_ranges, _) = parse_input("../inputs/5.txt");
+
+    let mut range_ends: Vec<RangeEnd> = vec![];
+
+    for id_range in id_ranges {
+        range_ends.push(RangeEnd {
+            value: id_range.min,
+            kind: RangeEndKind::Min,
+        });
+
+        range_ends.push(RangeEnd {
+            value: id_range.max,
+            kind: RangeEndKind::Max,
+        });
+    }
+
+    range_ends.sort_by(|a, b| a.value.cmp(&b.value));
+
+    let mut new_ranges: Vec<IdRange> = vec![];
+
+    let mut currently_tracking_min: Option<u64> = None;
+    let mut total_mins_visited = 0;
+    let mut total_maxes_visited = 0;
+
+    for range_end in range_ends {
+        if matches!(range_end.kind, RangeEndKind::Min) {
+            total_mins_visited += 1;
+            if currently_tracking_min == None {
+                currently_tracking_min = Some(range_end.value)
+            }
+        }
+
+        if matches!(range_end.kind, RangeEndKind::Max) {
+            total_maxes_visited += 1;
+
+            if total_maxes_visited == total_mins_visited && currently_tracking_min.is_some() {
+                let overlaps_previous_range = new_ranges.len() > 0
+                    && currently_tracking_min.unwrap() == new_ranges.last().unwrap().max;
+
+                if overlaps_previous_range {
+                    let previous_most_recent_range = new_ranges.pop();
+                    new_ranges.push(IdRange {
+                        min: previous_most_recent_range.unwrap().min,
+                        max: range_end.value,
+                    })
+                } else {
+                    new_ranges.push(IdRange {
+                        min: currently_tracking_min.unwrap(),
+                        max: range_end.value,
+                    });
+                }
+
+                currently_tracking_min = None;
+            }
+        }
+    }
+
+    let mut result: u64 = 0;
+
+    for id_range in new_ranges {
+        result += id_range.max - id_range.min + 1;
+    }
+
+    result
 }
 
 fn parse_input(filename: impl AsRef<Path>) -> (Vec<IdRange>, Vec<u64>) {
@@ -53,4 +116,14 @@ fn parse_id_range(raw_line: &str) -> IdRange {
 struct IdRange {
     min: u64,
     max: u64,
+}
+
+struct RangeEnd {
+    value: u64,
+    kind: RangeEndKind,
+}
+
+enum RangeEndKind {
+    Min,
+    Max,
 }
