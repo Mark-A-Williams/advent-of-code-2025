0a81234a2351b68369e761190c9b01c54ce6af5b
Mark Williams <amwilliams137@gmail.com>
2025-12-17 17:59:24 +0000

    4.2

--- aoc-2025-rust/src/solutions/day_4.rs
+++ aoc-2025-rust/src/solutions/day_4.rs
@@ -18,7 +18,7 @@ pub fn part_1() -> u32 {
         let mut x = 0;
 
         while x < width {
-            if grid[y][x] == '@' && !has_too_many_rolls_in_adjacent_positions(x, y, &grid) {
+            if grid[y][x] == '@' && is_position_accessible(x, y, &grid) {
                 accessible_rolls_count += 1;
             }
 
@@ -32,14 +32,54 @@ pub fn part_1() -> u32 {
 }
 
 pub fn part_2() -> u32 {
-    0
+    let lines = get_lines_from_file("../inputs/4.txt");
+
+    let width = lines.first().unwrap().len();
+    let height = lines.len();
+
+    let mut grid: Vec<Vec<char>> = lines
+        .into_iter()
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect();
+
+    let mut removed_rolls_count_total = 0;
+
+    loop {
+        let removed_count = remove_all_accessible_rolls(&mut grid, width, height);
+
+        if removed_count == 0 {
+            break;
+        }
+
+        removed_rolls_count_total += removed_count;
+    }
+
+    removed_rolls_count_total
 }
 
-fn has_too_many_rolls_in_adjacent_positions(
-    x_centre: usize,
-    y_centre: usize,
-    grid: &Vec<Vec<char>>,
-) -> bool {
+fn remove_all_accessible_rolls(grid: &mut Vec<Vec<char>>, width: usize, height: usize) -> u32 {
+    let mut removed_rolls_count = 0;
+
+    let mut y = 0;
+    while y < height {
+        let mut x = 0;
+
+        while x < width {
+            if grid[y][x] == '@' && is_position_accessible(x, y, &grid) {
+                grid[y][x] = '.';
+                removed_rolls_count += 1;
+            }
+
+            x += 1;
+        }
+
+        y += 1;
+    }
+
+    return removed_rolls_count;
+}
+
+fn is_position_accessible(x_centre: usize, y_centre: usize, grid: &Vec<Vec<char>>) -> bool {
     let mut adjacent_roll_count = 0;
 
     let mut y = if y_centre == 0 { 0 } else { y_centre - 1 };
@@ -53,7 +93,7 @@ fn has_too_many_rolls_in_adjacent_positions(
             }
 
             if adjacent_roll_count >= 4 {
-                return true;
+                return false;
             }
 
             x += 1;
@@ -62,7 +102,7 @@ fn has_too_many_rolls_in_adjacent_positions(
         y += 1;
     }
 
-    return false;
+    return true;
 }
 
 fn is_position_roll(x: usize, y: usize, grid: &Vec<Vec<char>>) -> bool {
